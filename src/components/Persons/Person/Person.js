510088f7df7293a78af758fbc93d28b7d7c11fcb
LizLiang <lizliang0112@gmail.com>
2018-11-20 19:15:53 +0800

    convert stateless component to stateful component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, {Component} from "react";
 import personStyle from "./Person.module.css";
 
-const Person = (props) => {
-    return (
-        <div className={personStyle.Person}>
-            <p onClick={props.click}>
-                I'm {props.name}, I'm {props.age}
-            </p>
-            <input type="text" onChange={props.changed} value={props.name}/>
-        </div>
-    );
-};
+class Person extends Component{
+    render(){ 
+        return (
+            <div className={personStyle.Person}>
+                <p onClick={this.props.click}>
+                    I'm {this.props.name}, I'm {this.props.age}
+                </p>
+                <input type="text" onChange={this.props.changed} value={this.props.name}/>
+            </div>
+        );
+    }
+}
 
 export default Person;
--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,16 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Person from "./Person/Person";
 
 // if we want to omit return keyword, we need to replace 
 // it with parentheses. but if we're not having a jsx
 // expression here, we also can omit parentheses. 
-const persons = (props) => props.persons.map((person, index) => {
-        return <Person
-        click={props.clicked.bind(this, index)}
-        name={person.name}
-        age={person.age}
-        key={person.id}
-        changed={(event) => { props.changed(event, person.id) }}/>
-      });
+class Persons extends Component{
+  render(){
+    return this.props.persons.map((person, index) => {
+      return <Person
+      click={this.props.clicked.bind(this, index)}
+      name={person.name}
+      age={person.age}
+      key={person.id}
+      changed={(event) => { this.props.changed(event, person.id) }}/>
+    });
+  }
+} 
 
-export default persons;
+export default Persons;
